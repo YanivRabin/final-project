9383ec1d9d642fd0b2d5532410bf16976e1fc333
YanivRabin <rabin.yaniv@gmail.com>
2024-05-11 16:19:47 +0800

    init typescript app

---
+++ server/src/common/auth_middleware.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+
+export interface AuthRequest extends Request {
+    user?: { _id: string };
+}
+
+const authenticate = (req: AuthRequest, res: Response, next: NextFunction) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Bearer <token>
+    if (!token){
+        return res.sendStatus(401);
+    }
+    jwt.verify(token, process.env.JWT_SECRET, (err, user: { _id: string }) => {
+        if (err) {
+            return res.sendStatus(401);
+        }
+        req.user = user;
+        next();
+    });
+}
+
+export default authenticate;
