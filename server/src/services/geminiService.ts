f7f47cb4bb6230bf664bf22155c6be1d35635bd3
Yehonatan Malki <joni910.malki@cs.colman.ac.il>
2024-06-01 19:13:50 +0300

    key

--- server/src/services/geminiService.ts
+++ server/src/services/geminiService.ts
@@ -4,7 +4,7 @@ import { WorkoutPlan } from '../model/workout_model';
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI("AIzaSyDg-m-XGj7-woIcJ_yy-NSnVM83XnQ6Ric");
+const genAI = new GoogleGenerativeAI(process.env.GENERATIVE_API_KEY);
 
 // ...
 
@@ -280,18 +280,3 @@ export const getResponseFromGemini = async () => {
     console.log(text);
     return result;
 }
-
-
-// async function run() {
-//     // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
-//     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-  
-//     const prompt = "Write a story about a magic backpack."
-  
-//     const result = await model.generateContent(prompt);
-//     const response = await result.response;
-//     const text = response.text();
-//     console.log(text);
-//   }
-  
-//   run();
